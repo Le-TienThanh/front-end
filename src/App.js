a9070ab2288c86e56ee85aabe732f4ac05e6fb95
Le Tien Thanh <letienthanhthptbs@gmail.com>
2025-04-17 08:47:52 +0700

    xong #30 nhung van con loi

--- src/App.js
+++ src/App.js
@@ -7,18 +7,58 @@ import { routes } from "./routes";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { isJsonString } from "./utils";
+import * as UserService from "./services/UserService";
+import { useDispatch } from "react-redux";
+import { updateUser } from "./redux/slides/userSlide";
+import { jwtDecode } from "jwt-decode";
 export function App() {
+  const dispatch = useDispatch();
+  const handleDecoded = () => {
+    let storageData = localStorage.getItem("access_token");
+    let decoded = {};
+    console.log("storageData", storageData, isJsonString(storageData));
+    if (storageData && isJsonString(storageData)) {
+      storageData = JSON.parse(storageData);
+      decoded = jwtDecode(storageData);
+    }
+    return { decoded, storageData };
+  };
+  useEffect(() => {
+    const { storageData, decoded } = handleDecoded();
+    if (decoded?.id) {
+      handleGetDetailsUser(decoded?.id, storageData);
+    }
+  }, []);
   
-  // useEffect(() => {
-  //   fetchApi();
-  // }, []);
+  UserService.axiosJwt.interceptors.request.use(async (config) => {
+    const currentTime = new Date()
+    const {  decoded } = handleDecoded();
+    if(decoded?.exp < currentTime.getTime() / 1000){
+      const data = await UserService.refreshToken();
+      config.headers["token"] = `Bearer ${data?.access_token}`;
+    }
+
+    
+    return config;
+  }, (err) => {
+    return Promise.reject(err);
+  }
+    
+   
+  );
+  const handleGetDetailsUser = async (id, token) => {
+    const res = await UserService.getDetailsUser(id, token);
+    dispatch(updateUser({ ...res?.data, access_token: token }));
+  };
+
   const fetchApi = async () => {
     const res = await axios.get(
       `${process.env.REACT_APP_API_URL}/product/get-all`
     );
     return res.data;
   };
-  const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi })
+  const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
   console.log("query", query);
 
   return (
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+export const axiosJwt = axios.create()
 
 
 export const loginUser = async (data ) => {
@@ -12,10 +13,18 @@ export const signupUser = async (data ) => {
 }
 
 export const getDetailsUser = async (id, access_token ) => {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/user/get-details/${id}`, {
+    const response = await axiosJwt.get(`${process.env.REACT_APP_API_URL}/user/get-details/${id}`, {
         headers: {
             token: `Bearer ${access_token}`
         }
+    })
+    return response.data
+}
+
+export const refreshToken = async ( ) => {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/user/refresh-token`, {
+        withCredentials: true,
+        
     })
     return response.data
 }
